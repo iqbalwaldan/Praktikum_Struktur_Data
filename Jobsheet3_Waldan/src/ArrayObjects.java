032eecce0b5e2ac9beb0e5ca3ec5d6d2c470e8b4
iqbalwaldan <iqbalwaldan0510@gmail.com>
2021-03-14 22:24:57 +0700

    Materi Praktikum Struktur Data Semester 2, Jobsheet3_Waldan

---
+++ Jobsheet3_Waldan/src/ArrayObjects.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author iqbal
+ */
+public class ArrayObjects {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan panjang array: ");
+        int n = sc.nextInt();
+        
+        PersegiPanjang[] ppArray = new PersegiPanjang[n];
+        
+        /*
+        ppArray[0] = new PersegiPanjang();
+        ppArray[0].panjang = 110;
+        ppArray[0].lebar = 30;
+
+        ppArray[1] = new PersegiPanjang();
+        ppArray[1].panjang = 80;
+        ppArray[1].lebar = 40;
+
+        ppArray[2] = new PersegiPanjang();
+        ppArray[2].panjang = 100;
+        ppArray[2].lebar = 20;
+         */
+        
+        for (int i = 0; i < 3; i++) {
+            ppArray[i] = new PersegiPanjang();
+            System.out.println("Persegi Panjang ke-" + i);
+            System.out.print("Masukkan Panjang: ");
+            ppArray[i].panjang = sc.nextInt();
+            System.out.print("Masukkan Lebar: ");
+            ppArray[i].lebar = sc.nextInt();
+        }
+
+        /*
+        System.out.println("Persegi Panjang ke-0, panjang: " + ppArray[0].panjang + ", lebar: " + ppArray[0].lebar);
+        System.out.println("Persegi Panjang ke-1, panjang: " + ppArray[1].panjang + ", lebar: " + ppArray[1].lebar);
+        System.out.println("Persegi Panjang ke-2, panjang: " + ppArray[2].panjang + ", lebar: " + ppArray[2].lebar);
+         */
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Persegi Panjang ke-" + i);
+            System.out.println("Panjang: " + ppArray[i].panjang + ", lebar: " + ppArray[i].lebar);
+        }
+    }
+}
